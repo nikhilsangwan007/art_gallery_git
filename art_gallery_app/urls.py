f835445e6155f634f20621f0357a5b08b169fb24
nikhilsangwan007 <nikhil.sangwan95@gmail.com>
2020-09-09 10:54:31 +0530

    done exhibition

--- art_gallery_app/urls.py
+++ art_gallery_app/urls.py
@@ -6,4 +6,6 @@ app_name = 'art_gallery_app'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('<int:pk>/exhibition', views.ExhibitionView.as_view(), name='exhibition'),
+    path('<int:pk>/collection', views.CollectionView.as_view(), name='collection'),
 ]
--- art_gallery_app/views.py
+++ art_gallery_app/views.py
@@ -12,4 +12,27 @@ class HomeView(generic.ListView):
 	def get_queryset(self):
 	    return DbGallery.objects.all()
 
+
+class ExhibitionView(generic.ListView):
+	template_name = 'art_gallery/exhibition.html'
+	context_object_name = 'exhibition_list'
+	
+	def get_queryset(self):
+		pk = self.kwargs['pk']
+		ex_list = DbExhibition.objects.filter(gallery=pk)
+		return ex_list
+
+class CollectionView(generic.ListView):
+	template_name = 'art_gallery/collection.html'
+	context_object_name = 'context_painting_list'
+
+	def get_queryset(self):
+		pk = self.kwargs['pk']
+		painting_id_list = DbExhibitionPainting.objects.filter(exhibition=pk)
+		context_painting_list = {}
+		for ids in painting_id_list:
+			painting_list = DbPainting.objects.filter(painting_id = ids.painting.painting_id)
+		context_painting_list['painting_list'] = painting_list
+		return context_painting_list
+
 # Create your views here.
