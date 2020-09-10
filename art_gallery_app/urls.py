1660a78fedc6fc9338b6c6d00603e384ffb2af0e
nikhilsangwan007 <nikhil.sangwan95@gmail.com>
2020-09-10 16:14:32 +0530

    final changes

--- art_gallery_app/urls.py
+++ art_gallery_app/urls.py
@@ -5,6 +5,9 @@ from . import views
 app_name = 'art_gallery_app'
 
 urlpatterns = [
+	path('register/', views.RegisterView, name='register'),
+	path('login/', views.LoginView, name='login'),
+	path('logout/', views.LogutUser, name='logout'),
     path('', views.HomeView.as_view(), name='home'),
     path('<int:pk>/exhibition', views.ExhibitionView.as_view(), name='exhibition'),
     path('<int:pk>/collection', views.CollectionView.as_view(), name='collection'),
